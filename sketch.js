9736d435c63850c8c1a200a56b1a37e59facb518
carlon-dart340 <113635760+carlon-dart340@users.noreply.github.com>
2022-10-12 15:52:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+let xpos = 0;
+let ypos = 0;
+let strokeweight=3;
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  background(100);
+
+ 
+  stroke(0, 160, 0);
+  strokeWeight(strokeweight);
+  line(250, 150, 150, 100);
+
+  
+  fill(255, 100, 0);
+  stroke(0, 100);
+  strokeWeight(strokeweight);
+
+  ellipse(250, 250, 400, 200);
+  ellipse(250, 250, 300, 200);
+  ellipse(250, 250, 200, 200);
+  ellipse(250, 250, 100, 200);
+  strokeweight= strokeweight + 1
+}
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  background(100);
+
+ 
+  stroke(0, 160, 0);
+  strokeWeight(strokeweight);
+  line(250, 150, 150, 100);
+
+  
+  fill(255, 100, 0);
+  stroke(0, 100);
+  strokeWeight(strokeweight);
+
+  ellipse(250, 250, 400, 200);
+  ellipse(250, 250, 300, 200);
+  ellipse(250, 250, 200, 200);
+  ellipse(250, 250, 100, 200);
+  strokeweight= strokeweight + .05
+}
